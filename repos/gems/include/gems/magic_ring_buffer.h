d4c69e443640c023ea96e7a8b160908eb8a0250f
Emery Hemingway <ehmry@posteo.net>
2018-04-04 17:42:10 +0200

    squash "Gems: magic ring buffer"
    Magic_ring_buffer: runtime capacity, managed dataspace
    
    Define the capacity of Magic_ring_buffer at runtime rather than as a
    template argument. Use an additional region map to reserve address space
    for a double mapped dataspace, which uses less memory than poking a hole
    with a double-sized dataspace. Test at 'run/magic_ring_buffer'.
    
    Ref #2725

--- repos/gems/include/gems/magic_ring_buffer.h
+++ repos/gems/include/gems/magic_ring_buffer.h
@@ -16,13 +16,21 @@
 
 /* Genode includes */
 #include <base/attached_ram_dataspace.h>
+#include <rm_session/connection.h>
+#include <region_map/client.h>
+#include <dataspace/client.h>
 
 namespace Genode {
-	template <typename TYPE, Genode::size_t SIZE>
+	template <typename TYPE>
 	struct Magic_ring_buffer;
 }
 
-template <typename TYPE, Genode::size_t SIZE>
+/**
+ * A ring buffer that uses a single dataspace mapped twice in consecutive
+ * regions. This allows any operation that is less or equal to the size of
+ * the buffer to be read or written in a single pass.
+ */
+template <typename TYPE>
 class Genode::Magic_ring_buffer
 {
 	private:
@@ -30,79 +38,71 @@ class Genode::Magic_ring_buffer
 		Magic_ring_buffer(Magic_ring_buffer const &);
 		Magic_ring_buffer &operator = (Magic_ring_buffer const &);
 
-		enum {
-			BUFFER_SIZE = align_addr(SIZE, 12),
-			CAPACITY = BUFFER_SIZE / sizeof(TYPE)
-		};
+		Genode::Env &_env;
 
-		Genode::Env &env;
+		Ram_dataspace_capability _buffer_ds;
 
-		addr_t map_first = 0;
-		addr_t map_second = 0;
+		size_t const _ds_size = Dataspace_client(_buffer_ds).size();
+		size_t const _capacity = _ds_size / sizeof(TYPE);
 
-		TYPE *buffer = nullptr;
+		Rm_connection _rm_connection { _env };
 
-		size_t wpos = 0;
-		size_t rpos = 0;
+		/* create region map (reserve address space) */
+		Region_map_client _rm { _rm_connection.create(_ds_size*2) };
 
-		Ram_dataspace_capability buffer_ds { };
+		/* attach map to global region map */
+		TYPE *_buffer = (TYPE *)_env.rm().attach(_rm.dataspace());
+
+		size_t _wpos = 0;
+		size_t _rpos = 0;
+
 
 	public:
 
 		/**
 		 * Ring capacity of TYPE items
 		 */
-		static size_t capacity() { return CAPACITY; }
+		size_t capacity() { return _capacity; }
 
 		/**
 		 * Constructor
 		 *
 		 * \param TYPE  Ring item type, size of type must be a
 		 *              power of two and less than the page size
-		 * \param SIZE  Size of items in ring, may be rounded up
-		 *              to the next page boundry
 		 *
-		 * \param env   Env for dataspace allocation and mapping
+		 * \param env       Env for dataspace allocation and mapping
+		 * \param num_bytes Size of ring in bytes, may be rounded up
+		 *                  to the next page boundry
 		 *
 		 * \throw Region_map::Region_conflict
 		 * \throw Out_of_ram
 		 * \throw Out_of_caps
 		 *
 		 */
-		Magic_ring_buffer(Genode::Env &env) : env(env)
+		Magic_ring_buffer(Genode::Env &env, size_t num_bytes)
+		: _env(env), _buffer_ds(_env.pd().alloc(num_bytes))
 		{
-			{
-				/* a hack to find the right sized void in the address space */
-				Attached_ram_dataspace filler(env.pd(), env.rm(), BUFFER_SIZE*2);
-				map_first = (addr_t)filler.local_addr<TYPE>();
-				map_second = map_first+BUFFER_SIZE;
+			if (_ds_size % sizeof(TYPE)) {
+				error("Magic_ring_buffer cannot hold unaligned TYPE");
+				throw Exception();
 			}
 
-			buffer_ds = env.pd().alloc(BUFFER_SIZE);
-
-			/* attach the buffer in two consecutive regions */
-			try {
-				map_first = env.rm().attach_at(buffer_ds, map_first, BUFFER_SIZE);
-				map_second = env.rm().attach_at(buffer_ds, map_second,  BUFFER_SIZE);
-			} catch (...) {
-				Genode::error("ring buffer dataspace double mapping failed");
-				if (map_first) env.rm().detach(map_first);
-				throw;
-			}
-
-			if ((map_first+BUFFER_SIZE) != map_second) {
-				error("failed to map ring buffer to consecutive regions");
-				throw Region_map::Region_conflict();
-			}
-
-			buffer = (TYPE *)map_first;
+			/* attach buffer dataspace twice into reserved region */
+			_rm.attach_at(_buffer_ds, 0, _ds_size);
+			_rm.attach_at(_buffer_ds, _ds_size, _ds_size);
 		}
 
 		~Magic_ring_buffer()
 		{
-			env.rm().detach(map_second);
-			env.rm().detach(map_first);
-			env.ram().free(buffer_ds);
+			/* detach dataspace from reserved region */
+			_rm.detach((addr_t)_ds_size);
+			_rm.detach((addr_t)0);
+
+			/* detach reserved region */
+			_env.rm().detach((addr_t)_buffer);
+
+			/* free buffer */
+			_env.ram().free(_buffer_ds);
 		}
 
 		/**
@@ -110,9 +110,12 @@ class Genode::Magic_ring_buffer
 		 */
 		size_t write_avail() const
 		{
-			if      (wpos > rpos) return ((rpos - wpos + CAPACITY) % CAPACITY) - 2;
-			else if (wpos < rpos) return rpos - wpos;
-			else                  return CAPACITY - 2;
+			if (_wpos > _rpos)
+				return ((_rpos - _wpos + _capacity) % _capacity) - 2;
+			else if (_wpos < _rpos)
+				return _rpos - _wpos;
+			else
+				return _capacity - 2;
 		}
 
 		/**
@@ -120,31 +123,33 @@ class Genode::Magic_ring_buffer
 		 */
 		size_t read_avail() const
 		{
-			if (wpos > rpos) return wpos - rpos;
-			else             return (wpos - rpos + CAPACITY) % CAPACITY;
+			if (_wpos > _rpos)
+				return _wpos - _rpos;
+			else
+				return (_wpos - _rpos + _capacity) % _capacity;
 		}
 
 		/**
 		 * Pointer to ring write address
 		 */
-		TYPE *write_addr() const { return &buffer[wpos]; }
+		TYPE *write_addr() const { return &_buffer[_wpos]; }
 
 		/**
 		 * Pointer to ring read address
 		 */
-		TYPE  *read_addr() const { return &buffer[rpos]; }
+		TYPE  *read_addr() const { return &_buffer[_rpos]; }
 
 		/**
 		 * Advance the ring write pointer
 		 */
 		void fill(size_t items) {
-			wpos = (wpos+items) % CAPACITY; }
+			_wpos = (_wpos+items) % _capacity; }
 
 		/**
 		 * Advance the ring read pointer
 		 */
 		void drain(size_t items) {
-			rpos = (rpos+items) % CAPACITY; }
+			_rpos = (_rpos+items) % _capacity; }
 };
 
 #endif
