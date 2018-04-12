c4f76ea09f0b21add9f2ef62ac37d75ff49f5f30
BugUser0815 <ga67wel@mytum.de>
2018-04-12 15:33:52 +0200

    Checkpoint restore extentions are not supposed to be stored here

--- repos/base-focnados/include/foc_native_cpu/client.h
+++ repos/base-focnados/include/foc_native_cpu/client.h
@@ -26,18 +26,12 @@ struct Genode::Foc_native_cpu_client : Rpc_client<Foc_native_cpu>
 	explicit Foc_native_cpu_client(Capability<Native_cpu> cap)
 	: Rpc_client<Foc_native_cpu>(static_cap_cast<Foc_native_cpu>(cap)) { }
 
-	Native_capability task_cap() override { return call<Rpc_task_cap>(); }
-
-	// START Modification for Checkpoint/Restore (rtcr)
-	addr_t cap_map_info() override { return call<Rpc_get_cap_map_info>(); }
-
-	void cap_map_info(addr_t addr) override { call<Rpc_set_cap_map_info>(addr); }
-
-	void install(Native_capability cap, addr_t kcap) override { call<Rpc_install>(cap, kcap); }
-	// END Modification for Checkpoint/Restore (rtcr)
+	Native_capability native_cap(Thread_capability cap) override {
+		return call<Rpc_native_cap>(cap); }
 
 	Foc_thread_state thread_state(Thread_capability cap) override {
 		return call<Rpc_thread_state>(cap); }
+
 };
 
 #endif /* _INCLUDE__FOC_NATIVE_CPU__CLIENT_H_ */
--- repos/base-focnados/include/foc_native_cpu/foc_native_cpu.h
+++ repos/base-focnados/include/foc_native_cpu/foc_native_cpu.h
@@ -26,25 +26,15 @@ struct Genode::Foc_native_cpu : Cpu_session::Native_cpu
 {
 	virtual Native_capability native_cap(Thread_capability) = 0;
 	virtual Foc_thread_state thread_state(Thread_capability) = 0;
-	// START Modification for Checkpoint/Restore (rtcr)
-	virtual void install(Native_capability, addr_t) = 0;
-	virtual addr_t cap_map_info() = 0;
-	virtual void cap_map_info(addr_t) = 0;
-	// END Modification for Checkpoint/Restore (rtcr)
-
-
+		
 	/*********************
 	 ** RPC declaration **
 	 *********************/
 
 	GENODE_RPC(Rpc_native_cap, Native_capability, native_cap, Thread_capability);
 	GENODE_RPC(Rpc_thread_state, Foc_thread_state, thread_state, Thread_capability);
-	// START Modification for Checkpoint/Restore (rtcr)
-	GENODE_RPC(Rpc_install, void, install, Native_capability, addr_t);
-	GENODE_RPC(Rpc_get_cap_map_info, addr_t, cap_map_info);
-	GENODE_RPC(Rpc_set_cap_map_info, void, cap_map_info, addr_t);
 
-	GENODE_RPC_INTERFACE(Rpc_native_cap, Rpc_thread_state, Rpc_install, Rpc_get_cap_map_info, Rpc_set_cap_map_info);
+	GENODE_RPC_INTERFACE(Rpc_native_cap, Rpc_thread_state);
 };
 
 #endif /* _INCLUDE__FOC_NATIVE_CPU__FOC_NATIVE_CPU_H_ */
